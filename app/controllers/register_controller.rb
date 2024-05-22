861191e5fd5cd01cc77cf3d4b536a5f58aa3b260
Josh Kennedy <976307+YoCodingJosh@users.noreply.github.com>
2024-05-21 22:03:06 -0500

    stub out the register controller some more

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -8,5 +8,25 @@ class RegisterController < ApplicationController
   end
 
   def register
+    # TODO: check if the user is already logged in and redirect them if they are.
+
+    @user = User.new(user_params)
+
+    if @user.invalid?
+      # TODO: display a flash message with the errors
+      render json: { errors: @user.errors }, status: :bad_request
+    end
+  end
+
+  private
+
+  def user_params
+    # strong parameters
+    params.require(:user).permit(
+      :username,
+      :email,
+      :password,
+      :password_confirmation
+    )
   end
 end
