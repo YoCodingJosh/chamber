b7dadc380b3f03073846ef870918cc6878630b07
Josh Kennedy <976307+YoCodingJosh@users.noreply.github.com>
2024-05-24 21:16:11 -0500

    fix register validation error text

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ class User < ApplicationRecord
   has_secure_password
 
   # Validate username
-  validates :username, presence: true, uniqueness: true, format: { with: /\A[a-zA-Z0-9_-]+\Z/, message: 'Invalid username!', length: { minimum: 3, maximum: 20, message: 'Incorrect username length!' } }
+  validates :username, presence: true, uniqueness: true, format: { with: /\A[a-zA-Z0-9_-]+\Z/, message: 'Invalid username!', length: { minimum: 3, maximum: 20 } }
 
   # Validate email
-  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'Invalid email address!' }
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 
   # Validate password
-  validates :password, presence: true, length: { minimum: 8, message: 'Incorrect password length!' }
+  validates :password, presence: true, length: { minimum: 8 }
 end
