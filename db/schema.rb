a4652544d7fa97c8e19f72e4dd7d4bae5af9ab86
Josh Kennedy <976307+YoCodingJosh@users.noreply.github.com>
2024-05-26 08:42:44 -0500

    add index for usernames

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_19_021703) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_26_120506) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_19_021703) do
     t.string "confirmation_token"
     t.datetime "confirmation_sent_at"
     t.datetime "confirmed_at"
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
